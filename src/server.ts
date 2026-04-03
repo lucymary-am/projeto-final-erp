22843e79d23ac1fd06cb9e1e926c03582d2e6430
lucymary-am <lucymary.am@gmail.com>
2026-04-02 20:28:07 -0400

    Ajustes do server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,40 @@
-import 'reflect-metadata';
-import express from 'express';
-import 'dotenv/config';
-import { appDataSource } from './database/appDataSource.js';
-import routes from "./routes/index.js";
+import "reflect-metadata";
+import express from "express";
+import "dotenv/config";
+import { appDataSource } from "./database/appDataSource.js";
+import { produtoRoutes } from "./routes/produtoRoutes.js";
+import { categoriaRoutes } from "./routes/categoriaRoutes.js";
+import { usuarioRoutes } from "./routes/usuarioRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(routes);
-
 app.use(express.json());
+
 // Rota de teste para confirmar que o servidor está funcionando
-app.get('/health', (_req, res) => {
-    res.json({ status: 'ok', mensagem: 'Servidor funcionando!' });
+app.get("/health", (_req, res) => {
+    res.json({ status: "ok", mensagem: "Servidor funcionando!" });
 });
 
-appDataSource.initialize()
+app.use(authRoutes);
+
+app.use("/produtos", produtoRoutes);
+app.use("/categorias", categoriaRoutes);
+app.use("/usuarios", usuarioRoutes);
+app.use("/login", authRoutes);
+app.use("/refresh", authRoutes);
+app.use("/logout", authRoutes);
+
+appDataSource
+    .initialize()
     .then(() => {
-    console.log('Banco de dados conectado!');
-    app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-})
-
-.catch((error: Error) => {
-    console.error('Erro ao conectar o banco:', error);
-});
+        console.log("Banco de dados conectado!");
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Erro ao conectar o banco:", error);
+    });
